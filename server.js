a3d9b2f8372ce8187267448ee45b7b47a70636d9
Satish Pokala <satishpokala124@gmail.com>
2020-08-04 18:49:39 +0530

    Updating Database Info

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-acute-36020',
     user : 'postgres',
     password : 'asdf1234',
     database : 'faceRecognition-db'
